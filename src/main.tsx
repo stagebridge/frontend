d779b276ba05444c25295b19b572ae63137d9995
HB Kwon <khbin04@gmail.com>
2025-10-29 14:32:07 +0900

    chore: 앱 구조 및 패키지 관련 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./app/router";
-import "./index.css";         // ✅ styles/ 없이 바로 index.css
+import router from "./app/router"; 
+import "./index.css";
 
 (() => {
   const saved = localStorage.getItem("sb_theme");
