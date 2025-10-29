bbd85414ef2690ac9e7d52c6f7618904f5038849
HB Kwon <khbin04@gmail.com>
2025-10-29 14:29:27 +0900

    refactor(ui): 기존 Navbar 구조 정리 및 불필요 파일 제거

--- src/components/navbar/SearchInput.tsx
+++
@@ -1,7 +0,0 @@
-export default function SearchInput() {
-  return (
-    <form role="search" onSubmit={(e) => e.preventDefault()}>
-      <input placeholder="한국과 일본의 콘서트를 한눈에!" />
-    </form>
-  );
-}
