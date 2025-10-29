9c3b378d75b270af27bf8dbda5f3c1698f1e2f17
HB Kwon <khbin04@gmail.com>
2025-10-29 14:31:47 +0900

    chore(config): 프로젝트 환경설정(Tailwind/tsconfig/vite) 업데이트

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+export default {
+  darkMode: "class", // ✅ 꼭 "class" 로 설정
+  content: ["./frontend/index.html", "./frontend/src/**/*.{ts,tsx,js,jsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
